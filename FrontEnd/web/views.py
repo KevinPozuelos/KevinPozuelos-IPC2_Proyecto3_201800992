99d903a0e7dee5c6a1931783e468b063a222cfef
KevinPozuelos <2723810200106@ingenieria.usac.edu.gt>
2021-05-04 20:10:07 -0600

    Frontend estilizado y actualizado

--- FrontEnd/web/views.py
+++ FrontEnd/web/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'web/home.html')
+
+def Peticion(request):
+    return render(request, 'web/Peticion.html')
     
